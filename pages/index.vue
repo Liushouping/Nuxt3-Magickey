9aa0d8e8e7003d53b6cade6916d6a9b2c4c76025
liushouping <kose146352@gmail.com>
2023-05-18 18:50:55 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -100,7 +100,7 @@ export default {
 }
 </script>
 <template>
-	<div class="bg-yellow-400 p-10 w-full h-screen">
+	<div class="bg-yellow-400 sm:p-10 w-full h-screen">
 
 	<div class="w-full h-full bg-gray-900 rounded-[5px] flex justify-center items-center">
 			
@@ -162,6 +162,7 @@ export default {
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
   @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
+  
   body,html {
   	font-family: 'Anton', sans-serif;
   };
